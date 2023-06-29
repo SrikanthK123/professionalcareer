f7e820d152b18b1dc52c2638c3b65e78e04b9009
SrikanthK123 <93971062+SrikanthK123@users.noreply.github.com>
2023-06-29 19:37:41 +0530

    changes in CSS

--- src/component/About.js
+++ src/component/About.js
@@ -6,14 +6,14 @@ const About = () => {
     <div className="home">
       <div className="nameTitle">
         <h4>About</h4>
-        <p>
+        <p className="p1">
           I'm an aspiring MERN stack developer, eager to kickstart my journey in
           the world of web development. Excited to share my passion for creating
           dynamic and interactive websites using the MERN stack.I have a solid
           foundation in the fundamental concepts of HTML, CSS, and JavaScript,
           which form the building blocks of web development.
         </p>
-        <p>
+        <p className="p2">
           In my learning journey, I have discovered the immense potential of the
           MERN stack, which combines MongoDB, Express.js, React.js, and Node.js.
           This powerful combination allows for the development of feature-rich
@@ -28,7 +28,7 @@ const About = () => {
           collaborate with others, contribute to open-source projects, and
           engage in continuous learning to refine my skills.
         </p>
-        <p>
+        <p className="p3">
           Please take a moment to browse through my portfolio to see the
           projects I've been working on. I'm thrilled to connect with fellow
           developers, potential collaborators, and mentors who share my passion
